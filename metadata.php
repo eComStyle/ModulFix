4d45d93667319e428772aa0fd06dba05076c93f7
puckl <mail@puckl.net>
2022-03-14 10:05:08 +0100

    style

--- metadata.php
+++ metadata.php
@@ -17,22 +17,21 @@
  *    along with this program.  If not, see {http://www.gnu.org/licenses/}.
  */
 
-$sMetadataVersion = '2.0';
-$aModule = array(
-    'id'                    => 'ecs_modulfix',
-    'title'                 => '<strong style="color:#04B431;">e</strong><strong>ComStyle.de</strong>:  <i>ModulFix</i>',
-    'description'           => 'Hilft, wenn Module nicht installiert werden koennen.',
-    'version'               => '1.0.0',
-    'thumbnail'             => 'ecs.png',
-    'author'                => '<strong style="font-size: 17px;color:#04B431;">e</strong><strong style="font-size: 16px;">ComStyle.de</strong>',
-    'email'                 => 'support@ecomstyle.de',
-    'devmail'               => 'dev@ecomstyle.de',
-    'url'                   => 'https://ecomstyle.de',
-    'extend' => array(
+$sMetadataVersion   = '2.0';
+$aModule            = [
+    'id'            => 'ecs_modulfix',
+    'title'         => '<strong style="color:#04B431;">e</strong><strong>ComStyle.de</strong>:  <i>ModulFix</i>',
+    'description'   => 'Hilft, wenn Module nicht installiert werden koennen.',
+    'version'       => '1.0.1',
+    'thumbnail'     => 'ecs.png',
+    'author'        => '<strong style="font-size: 17px;color:#04B431;">e</strong><strong style="font-size: 16px;">ComStyle.de</strong>',
+    'email'         => 'info@ecomstyle.de',
+    'url'           => 'https://ecomstyle.de',
+    'extend' => [
         \OxidEsales\Eshop\Application\Controller\Admin\ModuleMain::class => \Ecs\ModulFix\Controller\Admin\ModuleMain::class,
-    ),
-    'events' => array(
-        'onActivate'   => '\Ecs\ModulFix\Core\Events::onActivate',
-        'onDeactivate' => '\Ecs\ModulFix\Core\Events::onDeactivate',
-    ),
- );
+    ],
+    'events' => [
+        'onActivate'    => '\Ecs\ModulFix\Core\Events::onActivate',
+        'onDeactivate'  => '\Ecs\ModulFix\Core\Events::onDeactivate',
+    ],
+];
